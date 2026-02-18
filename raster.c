8915320f2c0b5e1e1d8cb4c09525572e33049841
Naveed Elias <nelia310@mtroyal.ca>
2026-02-17 19:20:47 -0700

    issue-13: Create plot_pixel - Moving work to home PC

--- raster.c
+++ raster.c
@@ -1,10 +1,23 @@
 #include "raster.h"
+#include <osbind.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 400
+#define BYTES_PER_SCREEN 32000
 
-void plot_pixel(char *base, int x, int y) {
-    if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT){
-        *(base + y * 80 + (x >> 3)) |= 1 << (7 - (x & 7));
+int main() {
+
+    	UINT8 *base = Physbase();
+        UINT16 row = 17;
+        UINT16 col = 26;
+
+        plot_pixel(base, row, col);
+
+    return 0;
+}
+
+void plot_pixel(UINT8 *base, UINT16 row, UINT16 col) {
+    if (row >= 0 && row < SCREEN_WIDTH && col >= 0 && col < SCREEN_HEIGHT){
+        *(base + col * 80 + (row >> 3)) |= 1 << (7 - (row & 7));
     }
 }
