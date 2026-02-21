ec1a1284332f144ec0c8796751346703fd1afa8e
ppraj069 <ppraj069@mtroyal.ca>
2026-02-20 23:09:09 -0700

    Issue 9: fixed the clear_region - The change made were:      - Loops were bound wrong. Since width !=abs row index      - Pointer types as start_byte/end_byte were 32 bits and curr was 8        bits that caused the incompitalbe warnings      - Mostly fixing small bugs

--- raster.c
+++ raster.c
@@ -1,18 +1,83 @@
 #include "raster.h"
 #include <osbind.h>
+#include <linea.h>
 
 #define SCREEN_BYTES_PER_ROW 80
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 400
 
 int main()
 {
-
+    /*
     UINT8 *base = Physbase();
     plot_bitmap_8(base, 100, 40, 8, smiley_bitmap);
     plot_bitmap_16((UINT16 *)base, 100, 80, 16, invader_bitmap);
-    plot_bitmap_32((UINT32 *)base, 100, 120, 32, block_bitmap);
+    plot_bitmap_32((UINT32 *)base, 100, 120, 32, block_bitmap);*/
+
+    UINT16 r = 0;
+    UINT16 c = 0;
+    UINT16 l = 40;
+    UINT16 w = 40;
+    UINT32 *base = Physbase();
+    clear_region(base, r, c, l, w);
 
     return 0;
 }
+void clear_region(UINT32 *base, UINT16 row, UINT16 col, UINT16 length, UINT16 width)
+{
+    UINT8 *base8 = (UINT8 *)base; /* cast to byte pointer for byte-level access */
+
+    UINT16 r;
+    UINT16 start_byte = col >> 3;             /*index of 1st byte in the row the region*/
+    UINT16 end_byte = (col + width - 1) >> 3; /*last byte index in the row the region*/
+
+    UINT8 start_bit = col & 7;             /*bit pos within the first byte where it starts*/
+    UINT8 end_bit = (col + width - 1) & 7; /*bit pos within the last byte where it ends */
+
+    for (r = row; r < row + length; r++)
+    {
+        UINT8 *line = base8 + (UINT32)r * SCREEN_BYTES_PER_ROW; /*pointer to the start of row r*/
+
+        UINT8 *start = line + start_byte; /*pointer to the first byte of the region on this row*/
+        UINT8 *end = line + end_byte;     /*pointer to the last byte of the region on this row */
+
+        UINT8 *curr = start;
+
+        while (curr <= end)
+        {
+            UINT8 mask;
+
+            if ((curr == start) && (curr == end))
+            {
+                /* region fits within a single byte, mask only the bits between start_bit and end_bit */
+                UINT8 left = 0xFF >> start_bit;      /*1s from start_bit to bit 0*/
+                UINT8 right = 0xFF << (7 - end_bit); /*1s from bit 7 to end_bit*/
+                mask = left & right;                 /*overlap gives us exactly what bits to clear*/
+                *curr &= ~mask;
+            }
+            else if (curr == start)
+            {
+                /* first byte: clear only from start_bit to the end of the byte */
+                mask = 0xFF >> start_bit;
+                *curr &= ~mask;
+            }
+            else if (curr == end)
+            {
+                /* last byte: clear only from the start of the byte to end_bit */
+                mask = 0xFF << (7 - end_bit);
+                *curr &= ~mask;
+            }
+            else
+            {
+                /*Clearing all bits in middle*/
+                *curr = 0x00;
+            }
+
+            curr++; /* move to the next byte */
+        }
+    }
+}
+
 void plot_bitmap_8(UINT8 *base, UINT16 row, UINT16 col, UINT16 height, const UINT8 *bitmap_8)
 {
     UINT16 byte_col = col >> 3; /*byte offset*/
@@ -102,5 +167,72 @@ void plot_bitmap_32(UINT32 *base, UINT16 row, UINT16 col, UINT16 height, const U
             dest[3] |= (b2 << (8 - bit_shift)) | (b3 >> bit_shift);
             dest[4] |= (b3 << (8 - bit_shift));
         }
+    }
+}
+
+void clear_screen(UINT32 *base)
+{ /*From Aidan's*/
+    int i;
+    UINT32 *loc;
+    i = 0;
+    loc = base;
+    while (i++ < LONGS_PER_SCREEN)
+        *(loc++) = 0;
+}
+
+void plot_pixel(UINT8 *base, UINT16 row, UINT16 col)
+{ /*from Naveed's PR*/
+    if (row >= 0 && row < SCREEN_WIDTH && col >= 0 && col < SCREEN_HEIGHT)
+    {
+        *(base + col * 80 + (row >> 3)) |= 1 << (7 - (row & 7));
+    }
+}
+
+void plot_character(UINT8 *base, UINT16 row, UINT16 col, char c)
+{
+    UINT8 *font;
+    UINT16 byte_col, bit_shift, r;
+    UINT32 offset;
+    UINT8 *dest;
+    UINT8 src;
+    /*Check if it is within the screen boundaries*/
+    if (row >= SCREEN_HEIGHT || col >= SCREEN_WIDTH)
+    {
+        return;
+    }
+    font = (UINT8 *)V_FNT_AD; /*Get the start add. of font table*/
+    byte_col = (UINT16)(col >> 3);
+    bit_shift = (UINT16)(col & 7);
+    for (r = 0; r < 16; r++)
+    {
+        if ((UINT16)(row + r) >= SCREEN_HEIGHT)
+        {
+            return;
+        }
+        /* row r of glyph c: font[c + 256 * r] ?//from lab*/
+        src = font[(UINT16)(UINT8)c + (UINT16)(256 * r)];
+        offset = (UINT32)(row + r) * SCREEN_BYTES_PER_ROW + byte_col;
+        dest = base + offset;
+        if (bit_shift == 0)
+        {
+            dest[0] |= src;
+        }
+        else
+        {
+            dest[0] |= (UINT8)(src >> bit_shift);
+            if (byte_col < (SCREEN_BYTES_PER_ROW - 1))
+            {
+                dest[1] |= (UINT8)(src << (8 - bit_shift));
+            }
+        }
+    }
+}
+
+void plot_string(UINT8 *base, UINT16 row, UINT16 col, char *str)
+{
+    while (*str)
+    {
+        plot_character(base, row, col, *str++);
+        col += 8; /*Move to the next character position*/
     }
 }
