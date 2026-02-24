fce57077eb87db5940fd3935283335d514a815a3
naveede1 <151809700+naveede1@users.noreply.github.com>
2026-02-23 22:47:53 -0700

    Issue 12 (#41)
    * issue-13: Transferring work to different device
    
    * issue-13: Create plot_pixel
    - Moving work to home PC
    
    * issue-13: Create plotting functions
    - Create plot_horizontal line
    - Create plot_vertical line
    - Create plot_line
    - No parameter checks for whether the arguements are greater then
      SCREEN_WIDTH AND SCREEN_HEIGHT, or less than 0
    
    NOTE: The plot line function uses Bresenham’s algorithm. I used
          a bot help me create the function, since figuring it out
          from sctach is not time efficient and a headache.
    
    * issue-12: Tranfer WIP to different device
    
    * issue-12: transferring WIP to different device
    - Swtich row and col for horizontal line calculations
    - Change typing from UINT32 to UNIT8 for start_byte_loc and
      end_byte_loc.
    - Create plot_rectangle
    - plot_rectangle draws a rectangle but the lines do not fully
      connect.
    
    * issue-12: Add plot_square
    
    * issue-12: Add plot_triangle
    
    * issue-12: Remove comments and main function
    
    * issue-12: Clean up code
    
    * issue-12: Clean up code
    
    ---------
    
    Co-authored-by: Naveed Elias <nelia310@mtroyal.ca>

--- raster.c
+++ raster.c
@@ -102,7 +102,7 @@ void plot_horizontal_line(UINT32 *base, UINT16 row, UINT16 col, UINT16 length) {
 }
 
 void plot_vertical_line(UINT32 *base, UINT16 row, UINT16 col, UINT16 length) {
-    UINT8 *byte_base = (UINT8 *)base; /* So we can work byte by byte */
+    UINT8 *byte_base = (UINT8 *)base;
     UINT16 i;
     UINT16 current_column;
     
@@ -147,5 +147,71 @@ void plot_line(UINT32 *base, UINT16 start_row, UINT16 start_col, UINT16 end_row,
             error += abs_distance_x;
             current_col += step_y;
         }
+    }
+}
+
+void plot_rectangle(UINT32 *base, UINT16 row, UINT16 col, UINT16 length, UINT16 width) {
+    UINT16 right_vertical_line_row = row + width - 1;    
+    UINT16 bottom_horizontal_line_col = col + length - 1;
+                                                  
+    plot_horizontal_line(base, row, col, width);
+    plot_horizontal_line(base, row, bottom_horizontal_line_col, width);
+    plot_vertical_line(base, row, col, length);
+    plot_vertical_line(base, right_vertical_line_row, col, length);
+}
+
+void plot_square(UINT32 *base, UINT16 row, UINT16 col, UINT16 side) {
+    UINT16 right_vertical_line_row = row + side - 1;    
+    UINT16 bottom_horizontal_line_col = col + side - 1;
+
+    plot_horizontal_line(base, row, col, side);
+    plot_horizontal_line(base, row, bottom_horizontal_line_col, side);
+    plot_vertical_line(base, row, col, side);
+    plot_vertical_line(base, right_vertical_line_row, col, side);
+}
+
+void plot_triangle(UINT32 *baseptr, UINT16 row, UINT16 col, UINT16 base, UINT16 height, UINT8 direction) {
+    UINT16 diag_start_row, diag_start_col;
+    UINT16 diag_end_row, diag_end_col;
+    
+    if (direction == 0) {
+        plot_vertical_line(baseptr, row, col, height);
+        plot_horizontal_line(baseptr, row, col, base);
+        
+        diag_start_row = row + base - 1;
+        diag_start_col = col;
+        diag_end_row = row;
+        diag_end_col = col + height - 1;
+        plot_line(baseptr, diag_start_row, diag_start_col, diag_end_row, diag_end_col);
+        
+    } else if (direction == 1) {
+        plot_vertical_line(baseptr, row, col, height);
+        plot_horizontal_line(baseptr, row - base + 1, col, base);
+        
+        diag_start_row = row - base + 1;
+        diag_start_col = col;
+        diag_end_row = row;
+        diag_end_col = col + height - 1;
+        plot_line(baseptr, diag_start_row, diag_start_col, diag_end_row, diag_end_col);
+        
+    } else if (direction == 2) {
+        plot_vertical_line(baseptr, row, col - height + 1, height);
+        plot_horizontal_line(baseptr, row, col, base);
+
+        diag_start_row = row + base - 1;
+        diag_start_col = col;
+        diag_end_row = row;
+        diag_end_col = col - height + 1;
+        plot_line(baseptr, diag_start_row, diag_start_col, diag_end_row, diag_end_col);
+        
+    } else if (direction == 3) {
+        plot_vertical_line(baseptr, row, col - height + 1, height);
+        plot_horizontal_line(baseptr, row - base + 1, col, base);
+        
+        diag_start_row = row - base + 1;
+        diag_start_col = col;
+        diag_end_row = row;
+        diag_end_col = col - height + 1;
+        plot_line(baseptr, diag_start_row, diag_start_col, diag_end_row, diag_end_col);
     }
 }
