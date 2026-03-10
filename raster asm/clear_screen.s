9ed4a1bff0d875b244e7ab98bdde83f74cb0786d
Naveed Elias <nelia310@mtroyal.ca>
2026-03-09 22:00:03 -0600

    issue-63: WIP, visibility push

--- raster asm/clear_screen.s
+++ raster asm/clear_screen.s
@@ -5,3 +5,15 @@
 ; INPUT: Address(UINT32*): to the start of the screen
 ;
 ; OUTPUT: None
+
+SCREEN_WIDTH	equ	8000
+BASE_32		equ	8
+
+clear_screen:
+		link	a6,#0
+		movem.l	d0-d2/a0-a2,-(sp)
+
+		clr 	d0
+		move.l	BASE_32(a6),a0
+
+		cmpi.w	#SCREEN_WIDTH,d0
