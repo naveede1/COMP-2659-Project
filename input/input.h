48b5aeaa0cb0c64fe59ceb33ba3a8b474638af0e
naveede1 <151809700+naveede1@users.noreply.github.com>
2026-03-12 13:31:19 -0600

    Issue 54: Input Module Part 1 (#69)
    * issue-54: Add input module
    
    - For the time being, input.h only includes the minimum functions
      stated in the checkpoint 3 document
    
    * issue-54: Implement has_input()
    
    * issue-54: Implement get_input()
    
    * issue-54: Add documentation and skeleton code
    
    ---------
    
    Co-authored-by: Naveed Elias <nelia310@mtroyal.ca>

---
+++ input/input.h
@@ -0,0 +1,53 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+typedef enum { FALSE, TRUE } boolean;
+
+/*----- Function: has_input -----
+
+PURPOSE: Checks if there is an input processed
+
+INPUT: NONE
+
+OUTPUT: TRUE if input was processed
+        FALSE if no input was processed
+
+*/
+boolean has_input();
+
+/*----- Function: get_input -----
+
+PURPOSE: Get input processed
+
+INPUT: NONE
+
+OUTPUT: Character processed
+
+*/
+char get_input();
+
+/*----- Function: is_held -----
+
+PURPOSE: Checks if the current input is being held
+
+INPUT: NONE
+
+OUTPUT: TRUE if the input processed was held
+        FALSE if not
+
+*/
+boolean is_held();
+
+/*----- Function: is_released -----
+
+PURPOSE: Check if the held input was released
+
+INPUT: NONE
+
+OUTPUT: TRUE if the held input was released
+        FALSE if the input is still being held
+
+*/
+boolean is_released();
+
+#endif
