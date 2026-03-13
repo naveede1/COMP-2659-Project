6c7e3d47ce7895e296a4252ebac8d4cb9a0bb988
ppraj069 <ppraj069@mtroyal.ca>
2026-03-12 19:15:53 -0600

    issue-70: Added the is_released function

--- input/input.c
+++ input/input.c
@@ -2,22 +2,44 @@
 #include "input.h"
 #include <osbind.h>
 
+static boolean held = FALSE;
+static boolean released = FALSE;
+
+
 int main() {
+    while(1){
+        if(has_input()){
+            char input = get_input();
+            printf("Input: %c\n", input);
+            
+            if(is_held()) printf("held\n");
+            if(is_released()) printf("released\n");
+            if(input =='q') break;        
+        }
+    }
     return 0;
 }
 
-boolean has_input() {
-    return (Cconis()) ? TRUE : FALSE;
+boolean has_input(){
+    return (Cconis() != 0) ? TRUE : FALSE;
 }
 
-char get_input() {
+char get_input() {    
+    held = TRUE;
+    released = FALSE;
     return Cnecin();
 }
 
-boolean is_held() {
-    return FALSE;
+boolean is_held(){
+    return held;
 }
 
-boolean is_released() {
+boolean is_released(){
+    if (held == TRUE) {
+        held = FALSE;
+        released = TRUE;
+        return TRUE;
+    }
+    released = FALSE;
     return FALSE;
 }
