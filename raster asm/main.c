af33c78485290e7067ddde3445d8fb8b8c2f1593
ppraj069 <ppraj069@mtroyal.ca>
2026-03-13 14:44:56 -0600

    issue-74: WIP
    - .s file is not compiling

--- raster asm/main.c
+++ raster asm/main.c
@@ -3,5 +3,11 @@
 #include <osbind.h>
 
 int main() {
-    return 0;
+    int *r = 10;
+    int *c = 10;
+    int *w = 32;
+    int *x_s = 0;
+    int *y_s = 0; 
+    int clip = clip_left_top_right_bottom(&r, &c, &w, 32, &x_s, &y_s);
+    return  clip;
 }
