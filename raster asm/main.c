9528a7e55b555281b0fc10b7caa1ad2b70bb313c
ppraj069 <ppraj069@mtroyal.ca>
2026-03-14 17:09:00 -0600

    issue-74: WTF

--- raster asm/main.c
+++ raster asm/main.c
@@ -3,11 +3,13 @@
 #include <osbind.h>
 
 int main() {
-    int *r = 10;
-    int *c = 10;
-    int *w = 32;
-    int *x_s = 0;
-    int *y_s = 0; 
-    int clip = clip_left_top_right_bottom(&r, &c, &w, 32, &x_s, &y_s);
-    return  clip;
+    int r = 10;
+    int c = 10;
+    int h = 32;
+    int x_s = 0;
+    int y_s = 0;
+
+    int clip = clip_left_top_right_bottom(&r, &c, &h, 32, &x_s, &y_s);
+    printf("clip = %d\n", clip);
+    return 0;   
 }
