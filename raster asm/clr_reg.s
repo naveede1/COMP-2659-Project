f864caf588b47afd22415fe7a7e86725e7adece3
ppraj069 <ppraj069@mtroyal.ca>
2026-03-15 23:09:35 -0600

    issue-64: WIP

--- raster asm/clr_reg.s
+++ raster asm/clr_reg.s
@@ -9,19 +9,20 @@
 ;        Width: the width (number of columns) in pixels of the region
 ;
 ; OUTPUT: None
+		
 		xdef _clear_region
 		xdef _clip_left_top_right_bottom
 
 SCREEN_BYTES_PER_ROW    equ     80
 
-; Argument offsets from a6
+; Stack offsets 
 base_ptr        equ      8
 row_arg         equ     12
 col_arg         equ     14
 length_arg      equ     16
 width_arg       equ     18
 
-; Local variable offsets from a6
+; Local variable offsets 
 r0_local        equ     -2
 c0_local        equ     -4
 h_local         equ     -6
@@ -34,7 +35,7 @@ _clear_region:
         link    a6,#-10
         movem.l d1-d7/a0-a4,-(sp)
 
-        ; --- Copy arguments into locals for clip call ---
+        ; Copy arguments into locals for clip call 
         move.w  row_arg(a6),d0
         move.w  col_arg(a6),d1
         move.w  length_arg(a6),d2
@@ -44,7 +45,7 @@ _clear_region:
         clr.w   xs_local(a6)
         clr.w   ys_local(a6)
 
-        ; --- Call clip_left_top_right_bottom ---
+        ; Call _clip_left_top_right_bottom 
         pea     ys_local(a6)                    ; &y_skip
         pea     xs_local(a6)                    ; &x_skip
         move.w  width_arg(a6),-(sp)             ; width
@@ -54,7 +55,7 @@ _clear_region:
         jsr     _clip_left_top_right_bottom
         add.l   #22,sp
 
-        ; --- Check visible width (d0) and clipped height ---
+        ; Check visible width (d0) and clipped height 
         tst.w   d0
         beq.s   clear_done
         move.w  h_local(a6),d2
@@ -64,9 +65,37 @@ _clear_region:
         ; d6 = visible width
         move.w  d0,d6
 
-        ; --- Reload clipped row/col ---
+        ; Reload clipped row/col 
         move.w  r0_local(a6),d4                 ; d4 = r0
         move.w  c0_local(a6),d5                 ; d5 = c0
+
+
+        ; start_byte = c0 >> 3 
+        move.w  d5,d0
+        lsr.w   #3,d0                           ; d0 = start_byte
+
+        ; end_byte = (c0 + visible - 1) >> 3 
+        move.w  d5,d1
+        add.w   d6,d1
+        subq.w  #1,d1
+        lsr.w   #3,d1                           ; d1 = end_byte
+
+        ; start_bit = c0 & 7
+        move.w  d5,d3
+        and.w   #7,d3                           ; d3 = start_bit
+
+        ; end_bit = (c0 + visible - 1) & 7 
+        move.w  d5,d7
+        add.w   d6,d7
+        subq.w  #1,d7
+        and.w   #7,d7                           ; d7 = end_bit
+        move.w  d7,ys_local(a6)                 ; save: high byte unused, low byte = end_bit
+
+        ; Base pointer 
+        movea.l base_ptr(a6),a0
+
+        clr.w   d5                              ; d5 = r = 0
+
 
 clear_done:
         movem.l (sp)+,d1-d7/a0-a4
