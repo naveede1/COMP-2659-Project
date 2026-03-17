1cf999c835bd819c0239ae8454eaeca42b426928
YQstudent <awojt835@mtroyal.ca>
2026-03-17 00:55:21 -0600

    issue-84: Fixing Score rendering - Adding intToString function - WARNING - Error with output string

--- obj Logic/rScore.c
+++ obj Logic/rScore.c
@@ -1,66 +1,41 @@
 #include "rScore.h"
 
+char* intToString(int value) {
+
+    static char score[7];  /* 6 digits + null terminator */
+    int divisor = 100000;  /* for 6-digit scores */
+    int i;
+
+    for (i = 0; i < 6; i++) {
+        int digit = value / divisor;
+        score[i] = '0' + digit;
+        value = value % divisor;
+        divisor /= 10;
+    }
+
+    score[6] = '\0'; /* null terminate the string */
+    return score;
+}
+
 void renderScore(Score score, UINT32 base) {
 
-    /*
-    char highScore[12];
-    char currScore{12};
-    */
-
-    linea0();
+    char* highScore;
+    char* currScore;
     
+    linea0();
 
     if (score.visible == 1){
 
+        printf("%s\n", intToString(score.value));
+
         plot_string(base, score.posY, score.posX, "HIGH SCORE");
-    
-        /*
-        score_int_to_string(score.highScore, highScore);
-        plot_string(base, score.posY + 15, score.posX + 16, highScore);
+        
+        currScore = intToString(score.value);
+        plot_string(base, score.posY+16, score.posX-62, currScore);
 
-        score_int_to_string(score.value, currScore);
-        plot_string(base, 186, 32, currScore);
-        */
+        highScore = intToString(score.highValue);
+        plot_string(base, score.posY+16, score.posX+16, highScore);
+
     }
 }
 
-void score_int_to_string(INT32 value, char *buffer) {
-    INT32 temp;
-    INT16 i = 0;
-    INT16 j;
-    INT16 is_negative = 0;
-
-    if (value == 0)
-    {
-        buffer[0] = '0';
-        buffer[1] = '\0';
-        return;
-    }
-
-    if (value < 0)
-    {
-        is_negative = 1;
-        value = -value;
-    }
-
-    /* Convert digits (reversed order) */
-    while (value > 0)
-    {
-        temp = value % 10;
-        buffer[i++] = (char)('0' + temp);
-        value = value / 10;
-    }
-
-    if (is_negative)
-        buffer[i++] = '-';
-
-    buffer[i] = '\0';
-
-    /* Reverse the string */
-    for (j = 0; j < i / 2; j++)
-    {
-        char t = buffer[j];
-        buffer[j] = buffer[i - j - 1];
-        buffer[i - j - 1] = t;
-    }
-}
--- obj Logic/score.h
+++ obj Logic/score.h
@@ -5,8 +5,8 @@ typedef struct { /* Structure for the Score Counter */
 
     int visible; /* No = 0, Yes = 1 */
     int posX, posY; /* Current Position */
-    long value; /* Current Score */
-    long highScore; /* High Score */
+    int value; /* Current Score */
+    int highValue; /* High Score */
 
 } Score;
 
