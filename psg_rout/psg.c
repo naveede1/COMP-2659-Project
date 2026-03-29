a579c3ca1dbd9c8ddd95de5b9b52847c4b4daae3
Naveed Elias <nelia310@mtroyal.ca>
2026-03-28 18:50:55 -0600

    issue-106: WIP, device transfer

--- psg_rout/psg.c
+++ psg_rout/psg.c
@@ -1,25 +1,124 @@
 #include "psg.h"
 
+/* 
+
+TO DO:
+
+    **Each function must validate its input parameters. If an input value is out-of-range, 
+    the function must ignore the request.
+
+    **Change typings to UINT8 where needed
+
+    **According to Parker using write_psg is slower then just:
+        
+        *PSG_reg_select = reg;
+	    *PSG_reg_write  = val;
+
+*/
+
+volatile unsigned char *PSG_reg_select = 0xFF8800; 
+volatile unsigned char *PSG_reg_write = 0xFF8802;
+
 void write_psg(int reg, UINT8 val) {
 
+    /* 
+    
+    Add arguement check:
+
+    Only 16 registers and only byte values between 0-255
+    
+    */
+
+    *PSG_reg_select = reg;
+	*PSG_reg_write  = val;
 }
 
-void read_psg(int reg) {
+UINT8 read_psg(int reg) {
 
+    /* 
+    
+    Add arguement check:
+
+    Limited amount of registers, between 0-F which is 1-16 
+    
+    */
+
+    *PSG_reg_select = reg;
+    return *PSG_reg_select;
 }
 
 void set_tone(int channel, int tuning) {
+
+    /*
+    
+    Add arguement checks
+    
+    */
+
+    int fine_tone;
+    int rough_tone;
+
+    fine_tone = tuning & 0xFF;
+    rough_tone = tuning >> 8;
+    
+    if (channel == 0) {
+        write_psg(0, fine_tone);
+        write_psg(1, rough_tone);
+    } else if (channel == 1) {
+        write_psg(2, fine_tone);
+        write_psg(3, rough_tone);
+    } else {
+        write_psg(4, fine_tone);
+        write_psg(5, rough_tone);
+    }
 
 }
 
 void set_volume(int channel, int volume) {
 
+    /*
+    
+    Add arguement checks
+
+    */
+
+    volume = volume & 0x0F;
+
+    if (channel == 0) {
+        write_psg(8, volume);
+    } else if (channel == 1) {
+        write_psg(9, volume);
+    } else {
+        write_psg(10, volume);
+    }
 }
 
 void enable_channels(int channel, int tone_on, int noise_on) {
 
+    UINT8 reg_7_value;
+    UINT8 tone;
+    UINT8 noise;
+
+    reg_7_value = read_psg(7);
+
+    tone &= ~(1 << channel); /* Tone bit position depending on the channel */
+    noise &= ~(1 << (channel + 3)); /* Noise bit position depending on the channel */
+
+    /* Set tone bit */
+    if (tone_on == 0) {
+        reg_7_value |= (1 << channel);
+    }
+
+    /* Set tone bit */
+    if (noise_on == 0) {
+        reg_7_value |= (1 << (channel + 3));
+    }
+
+    write_psg(7, reg_7_value);
 }
 
 void stop_sound() {
-
+    write_psg(8, 0);
+    write_psg(9, 0);
+    write_psg(10, 0);  
 }
---
+++ psg_rout/psg_tst.c
@@ -0,0 +1,27 @@
+#include "psg.c"
+
+int main() {
+	long old_ssp = Super(0);
+
+	*PSG_reg_select = 0;		/* set channel A fine tune = 248 */
+	*PSG_reg_write  = 248;
+
+	*PSG_reg_select = 1;		/* set channel A coarse tune = 0 */
+	*PSG_reg_write  = 0;
+
+	*PSG_reg_select = 7;		/* enable channel A on mixer */
+	*PSG_reg_write  = 0x3E;
+
+	*PSG_reg_select = 8;		/* set channel A volume = 11 */
+	*PSG_reg_write  = 11;
+
+	while (!Cconis())		/* tone now playing, await key */
+		;
+
+	*PSG_reg_select = 8;		/* set channel A volume = 0 */
+	*PSG_reg_write  = 0;
+
+	Cnecin();
+	Super(old_ssp);
+	return 0;
+}
